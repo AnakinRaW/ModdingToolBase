73417cfa51010df82192eb143ab8b051e7638722
AnakinRaW <anakin@gmx-topmail.de>
2026-05-24 13:40:59 +0200

    to string API

--- src/AnakinApps/ApplicationBase/Update/CosturaLockedFileHandler.cs
+++ src/AnakinApps/ApplicationBase/Update/CosturaLockedFileHandler.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO.Abstractions;
 using AnakinRaW.AppUpdaterFramework.Handlers.Interaction;
 
 namespace AnakinRaW.ApplicationBase.Update;
 
 public sealed class CosturaLockedFileHandler : ILockedFileInteractionHandler
 {
-    public LockedFileHandlerInteractionResult HandleLockedFile(IFileInfo file, IEnumerable<ILockingProcess> lockingProcesses)
+    public LockedFileHandlerInteractionResult HandleLockedFile(string file, IEnumerable<ILockingProcess> lockingProcesses)
     {
         throw new InvalidOperationException("Costura Applications do not support locked files.");
     }
